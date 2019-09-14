408e8b6ba41e0c8fa06f75db557d5c4bea45ebab
Deinyefa <df.eporwei@gmail.com>
2019-09-14 10:00:29 -0400

    adding migrations

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ INSERT INTO todos (description) VALUES ('Another thing');
 INSERT INTO todos (description) VALUES ('And another thing');
 ```
 update username in database uri to `deiny`
+
+## Migrations
+install `flask-migrate` with `pip install Flask-Migrate`
+
+- run `flask db init` to generate initial migrations folder structure 
+- `dropdb todoapp` to reset it
+- `createdb todoapp` 
+- run `flask db migrate` to sync models. Detects changes to the model and creates a migration file with upgrade and downgrade logic set up.
--- app.py
+++ app.py
@@ -1,37 +1,48 @@
-# import psycopg2
+import sys
+
 from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://deiny@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id}, {self.description}>'
 
 
-db.create_all()
-
-
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    # get the input user sent
-    description = request.get_json()['description']
-    # create a new pending addition to todos table
-    todo = Todo(description=description)
-    db.session.add(todo)
-    # commit that to the db
-    db.session.commit()
-    # redirect user back to index page
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        # get the input user sent
+        description = request.get_json()['description']
+        # create a new pending addition to todos table
+        todo = Todo(description=description)
+        db.session.add(todo)
+        # commit that to the db
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        # redirect user back to index page
+        return jsonify(body)
 
 
 @app.route('/')
