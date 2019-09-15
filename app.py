ded0dfb079e118bb4e0dcd7cad269a8edd5e0cb7
Deinyefa <df.eporwei@gmail.com>
2019-09-15 16:12:07 -0400

    Relationships - one to many: having todos categorized into lists

--- app.py
+++ app.py
@@ -11,11 +11,20 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey(
+        'todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id}, {self.description}>'
@@ -71,6 +80,14 @@ def remove_item(todo_id):
     return jsonify({'success': True})
 
 
+@app.route('/lists/<list_id>')
+def get_todos_list(list_id):
+    return render_template('index.html',
+                           lists=TodoList.query.order_by('id').all(),
+                           active_list=TodoList.query.get(list_id),
+                           todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_todos_list', list_id=1))
